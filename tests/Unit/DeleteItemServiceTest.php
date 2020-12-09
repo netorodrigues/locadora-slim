7934949d0275a2a22d5b7f31405a935696618476
Neto Rodrigues <jrodrigueson@gmail.com>
2020-12-09 19:53:23 -0300

    adds delete tests

---
+++ tests/Unit/DeleteItemServiceTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare (strict_types = 1);
+
+use App\Factories\Contracts\ItemFactory;
+use App\Services\Item\Contracts\CreateItemServiceInterface;
+use App\Services\Item\Contracts\DeleteItemServiceInterface;
+use Tests\BaseTest;
+
+class DeleteItemServiceTest extends BaseTest
+{
+    private $deleteItemService;
+    private $createItemService;
+    private $itemFactory;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->createItemService = $this->container->get(
+            CreateItemServiceInterface::class
+        );
+
+        $this->deleteItemService = $this->container->get(
+            DeleteItemServiceInterface::class
+        );
+
+        $this->itemFactory = $this->container->get(
+            ItemFactory::class
+        );
+    }
+
+    private function createItem()
+    {
+        $itemType = 'book';
+        $itemName = 'some-book-name';
+        $itemAvailable = true;
+
+        $item = $this->itemFactory->fromArray(
+            array(
+                'name' => $itemName,
+                'type' => $itemType,
+                'available' => $itemAvailable,
+            )
+        );
+
+        return $this->createItemService->execute($item);
+    }
+
+    public function testDeleteItem()
+    {
+        $item = $this->createItem();
+        $itemId = $item->getId()->getValue();
+
+        $wasDeleted = $this->deleteItemService->execute(
+            $itemId
+        );
+
+        $this->assertTrue($wasDeleted);
+
+    }
+
+    public function testDeleteItemWithInvalidId()
+    {
+        $invalidId = 'some-invalid-id';
+
+        $wasDeleted = $this->deleteItemService->execute($invalidId);
+
+        $this->assertTrue($wasDeleted);
+    }
+}
