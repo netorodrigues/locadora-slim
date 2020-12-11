a6bbdad13138d2766fe47f2c87eecfa30209b53b
Neto Rodrigues <jrodrigueson@gmail.com>
2020-12-11 15:49:44 -0300

    tries to parse all request bodys in json body parser

--- app/Middlewares/JSONBodyParserMiddleware.php
+++ app/Middlewares/JSONBodyParserMiddleware.php
@@ -11,13 +11,8 @@ class JSONBodyParserMiddleware
 {
     public function __invoke(Request $request, RequestHandler $handler): Response
     {
-        $contentType = $request->getHeaderLine('Content-Type');
-        if (strstr($contentType, 'application/json')) {
-            $contents = json_decode(file_get_contents('php://input'), true);
-            if (json_last_error() === JSON_ERROR_NONE) {
-                $request = $request->withParsedBody($contents);
-            }
-        }
+        $contents = json_decode(file_get_contents('php://input'), true);
+        $request = $request->withParsedBody($contents);
 
         return $handler->handle($request);
     }
