3980577fed5fe4b2470ecffd8683b32e8560bbcd
Neto Rodrigues <jrodrigueson@gmail.com>
2020-12-11 14:09:06 -0300

    adds strict types to exceptions

--- app/Exceptions/ItemDoesntExistsException.php
+++ app/Exceptions/ItemDoesntExistsException.php
@@ -1,4 +1,6 @@
 <?php
+
+declare (strict_types = 1);
 namespace App\Exceptions;
 
 final class ItemDoesntExistsException extends APIException
--- app/Exceptions/ValueObjects/InvalidItemTypeReceivedException.php
+++ app/Exceptions/ValueObjects/InvalidItemTypeReceivedException.php
@@ -1,4 +1,6 @@
 <?php
+
+declare (strict_types = 1);
 namespace App\Exceptions\ValueObjects;
 
 use App\Entities\ValueObjects\ItemType;
